88380c178064796996ef6de24aadcf56a500d330
tako2039 <takeo2039@gmail.com>
2022-03-31 22:48:05 -0500

    craete file

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    public static void main (String[] args) {
+        System.out.println("Hello World!");
+
+        int myFirstNumber = (10 + 5) + (10 * 2);
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        System.out.println(myFirstNumber);
+
+    }
+}
